dd1dec6c164421113ab3bbc9826c62f0bad6b04e
Understoe <marie.pierre987@gmail.com>
2025-05-15 14:35:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const TAGS = {
     "MTG : Standard": "<@&1067857092561608764>",
     "MTG : Pioneer": "<@&1067857419369205951>",
     "MTG : Duel Commander": "<@&@1067857231288217681>",
-    "MTG : Pauper": "<@&1372549642667167804>",
+    "MTG : Pauper": "<@&1067857461417099314>",
     "MTG : Limité": "<@&1067857130176118855>"
 };
 
@@ -63,7 +63,7 @@ client.once("ready", async () => {
     const events = await scrapeEvents();
 
     for (const event of events.reverse()) {
-        const content = `${event.role} [**${event.title}**](${event.link})\n📅 ${event.day} ${event.month}`;
+        const content = `[**${event.title}**](${event.link})\n${event.role}\n📅 ${event.day} ${event.month}`;
         await channel.send({ content, files: event.imageUrl ? [event.imageUrl] : [] });
     }
 
